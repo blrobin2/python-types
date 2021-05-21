5fd3e95ee04c15d303723a876623243b97d57962
Bennie Robinson <bennrobi@cisco.com>
2021-05-21 14:24:37 -0400

    different types

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 def get_full_name(first_name: str, last_name: str):
-  return first_name.title() + " " + last_name.title()
+    return first_name.title() + " " + last_name.title()
+
+
+def get_name_with_age(name: str, age: int):
+    return name + " is this old: " + str(age)
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from app import get_full_name
+from app import get_full_name, get_name_with_age
+
 
 def test_full_name():
-  assert get_full_name('john', 'doe') == 'John Doe'
+    assert get_full_name('john', 'doe') == 'John Doe'
+
+
+def test_name_with_age():
+    assert get_name_with_age('john', 8) == 'john is this old: 8'
